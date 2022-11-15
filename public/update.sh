2ef5dfc98b550d737976f4772f8d2494ee193e86
Murilo Polese <murilopolese@gmail.com>
2022-11-15 18:16:22 +0100

    Update directory and backend url

--- public/update.sh
+++ public/update.sh
@@ -1,15 +1,15 @@
 echo "Heating up server!"
 
-curl https://punanimation-directory.herokuapp.com
+curl https://panimation-directory.dokku.bananabanana.me/
 
 echo "Downloading entries"
-curl https://punanimation-directory.herokuapp.com/api/entries -o public/entries.json
+curl https://panimation-directory.dokku.bananabanana.me/api/entries -o public/entries.json
 echo "Downloading locations"
-curl https://punanimation-directory.herokuapp.com/api/locations -o public/locations.json
+curl https://panimation-directory.dokku.bananabanana.me/api/locations -o public/locations.json
 echo "Downloading skills"
-curl https://punanimation-directory.herokuapp.com/api/skills -o public/skills.json
+curl https://panimation-directory.dokku.bananabanana.me/api/skills -o public/skills.json
 echo "Downloading softwares"
-curl https://punanimation-directory.herokuapp.com/api/softwares -o public/softwares.json
+curl https://panimation-directory.dokku.bananabanana.me/api/softwares -o public/softwares.json
 
 echo "Rebuild frontend"
 npm run build
